e58c81327ff1a1269b4663e4e4494cba13e6d3e5
Oleksandr Moskalenko <43320447+thelesik@users.noreply.github.com>
2022-04-21 15:35:53 -0500

    Update workfile.sh

--- workfile.sh
+++ workfile.sh
@@ -1,4 +1,9 @@
-aws_access_key_id=""
-aws_secret_access_key=""
+aws_access_key_id="123123"
+aws_secret_access_key="123123"
+aws_output_format="yaml"
 region1="us-east-1"
 region2="us-east-2"
+aws configure set aws_access_key_id $aws_access_key_id;
+aws configure set aws_secret_access_key $aws_secret_access_key;
+aws configure set default.region $region1;
+aws configure set output $aws_output_format;
