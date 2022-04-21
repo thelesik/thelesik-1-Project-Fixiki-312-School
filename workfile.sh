8b5349291e0556d7bfd26cf9edacd9921bf6eb5d
Oleksandr Moskalenko <43320447+thelesik@users.noreply.github.com>
2022-04-21 16:56:21 -0500

    Update workfile.sh

--- workfile.sh
+++ workfile.sh
@@ -7,3 +7,8 @@ aws configure set aws_access_key_id $aws_access_key_id;
 aws configure set aws_secret_access_key $aws_secret_access_key;
 aws configure set default.region $region1;
 aws configure set output $aws_output_format;
+aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem
+chmod 400 MyKeyPair.pem;
+
+        #out-file -encoding ascii -filepath ~/MyKeyPair.pem
+#:aws ec2 create-security-group --group-name NewsgGroup12;
