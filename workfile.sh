e89a89d61541109e5989ca623aeca72dd35c34c9
Oleksandr Moskalenko <43320447+thelesik@users.noreply.github.com>
2022-04-21 22:44:34 -0500

    Update workfile.sh

--- workfile.sh
+++ workfile.sh
@@ -10,5 +10,18 @@ aws configure set output $aws_output_format;
 aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem
 chmod 400 MyKeyPair.pem;
 sgid=$(aws ec2 create-security-group --description newsgforcli --group-name NewsgGroup12 | jq -r '.GroupId'); #need to find id of thias SG:
-subnetid=$(aws ec2 describe-subnets | jq -r '.Subnets[0].SubnetId');
+subnetid=$(aws ec2 describe-subnets | jq -r '.Subnets[1].SubnetId');
 aws ec2 run-instances --image-id ami-0f9fc25dd2506cf6d --instance-type t2.nano --count 1 --subnet-id $subnetid --security-group-ids $sgid --key-name MyKeyPair > logs.json;
+
+#
+#
+#
+#
+#
+#
+#
+#
+#CODE FOR WEB SERVER
+sleep 30;
+instanceid=$(jq -r '.Instances[0].InstanceId' logs.json);
+aws ec2 create-image --instance-id $instanceid --name NewAMIforclitask1awsadvanced123;
