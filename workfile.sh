40c6cc27c4520ed2cf0893ccb714d6b877eb0e5f
Oleksandr Moskalenko <43320447+thelesik@users.noreply.github.com>
2022-04-21 22:21:38 -0500

    Update workfile.sh

--- workfile.sh
+++ workfile.sh
@@ -1,6 +1,6 @@
 aws_access_key_id=""
 aws_secret_access_key=""
-aws_output_format="yaml"
+aws_output_format="json"
 region1="us-east-1"
 region2="us-east-2"
 aws configure set aws_access_key_id $aws_access_key_id;
@@ -9,17 +9,6 @@ aws configure set default.region $region1;
 aws configure set output $aws_output_format;
 aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem
 chmod 400 MyKeyPair.pem;
-
-        #out-file -encoding ascii -filepath ~/MyKeyPair.pem
-#:aws ec2 create-security-group --group-name NewsgGroup12;
-
-\\
-aws configure set aws_access_key_id $aws_access_key_id;
-aws configure set aws_secret_access_key $aws_secret_access_key;
-aws configure set default.region $region1;
-aws configure set output $aws_output_format;
-aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem
-chmod 400 MyKeyPair.pem;
-aws ec2 create-security-group --group-name NewsgGroup12; #need to find id of thias SG:
-ec2 run-instances --image-id ami-0f9fc25dd2506cf6d --instance-type t2.nano --count 1 --subnet-id us-east-1a --security-group-ids  --key-name <name of key value pair>
-.
+sgid=$(aws ec2 create-security-group --description newsgforcli --group-name NewsgGroup12 | jq -r '.GroupId'); #need to find id of thias SG:
+subnetid=$(aws ec2 describe-subnets | jq -r '.Subnets[0].SubnetId');
+aws ec2 run-instances --image-id ami-0f9fc25dd2506cf6d --instance-type t2.nano --count 1 --subnet-id $subnetid --security-group-ids $sgid --key-name MyKeyPair > logs.json;
