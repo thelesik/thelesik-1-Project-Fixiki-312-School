eefbf63d8310e7ebd08b120bb15e81c13448398d
Oleksandr Moskalenko <43320447+thelesik@users.noreply.github.com>
2022-04-22 23:32:23 -0500

    Update complete.sh

--- complete.sh
+++ complete.sh
@@ -1,5 +1,5 @@
-aws_access_key_id="AKIAVZGV6CUEXTI7HV5R"
-aws_secret_access_key="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"
+aws_access_key_id=""
+aws_secret_access_key=""
 aws_output_format="json"
 region1="us-east-1"
 region2="us-east-2"
