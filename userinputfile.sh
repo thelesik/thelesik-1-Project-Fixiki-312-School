ed30e1fb68468fac0db9e812dadf0764a8209f3d
Oleksandr Moskalenko <43320447+thelesik@users.noreply.github.com>
2022-04-30 18:15:43 -0500

    Create userinputfile.sh

---
+++ userinputfile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+aws_access_key_id="";
+aws_secret_access_key="";
+aws_output_format="json";
+region1="us-east-1";
+region2="us-east-2";
+keyname="${region1}DefaultKeyName123321123321123321";
+keyname2="${region2}DefaultKeyName123321123321123321";
+#serverdata=$(echo serverdata.txt);
